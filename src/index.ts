86ad5c91c58b6ac855ada6263ed51f1575e38a61
Alexandria Pagram <alexandriagriffiths@yahoo.com.au>
2021-08-28 23:50:51 +1000

    Working tutorial, with custom names

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
 
-var zoneTCMName = "popupTCMZone";
+var zoneTCMName = "popupTutorialZone"; // on your special layer with zone tiles, this is the name you've given to your zone special property on that layer
 
 var currentPopup : any = undefined;
 
 WA.onEnterZone(zoneTCMName, () => {
-    currentPopup =  WA.openPopup("popUpTCM","Hold SHIFT to run ! " +
+    // popUpTutorial -> rect object name on floor layer
+    currentPopup =  WA.openPopup("popUpTutorialRect","Hold SHIFT to run ! " +
         " Up to 4 people at a time can join proximity chat - otherwise for bigger groups join a jitsi call by hanging around the tables.",[]);
 })
 
