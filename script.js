1b5b0e9094a4e054111322bba9a0c5f8a314cb62
alexandria-p <alexandriagriffiths@yahoo.com.au>
2021-08-28 13:51:32 +0000

    Deploying to gh-pages from master @ 86ad5c91c58b6ac855ada6263ed51f1575e38a61 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";var o="popupTCMZone",e=void 0;WA.onEnterZone(o,(()=>{e=WA.openPopup("popUpTCM","Hold SHIFT to run !  Up to 4 people at a time can join proximity chat - otherwise for bigger groups join a jitsi call by hanging around the tables.",[])})),WA.onLeaveZone(o,(function(){void 0!==e&&(e.close(),e=void 0)}))})();
+(()=>{"use strict";var o="popupTutorialZone",e=void 0;WA.onEnterZone(o,(()=>{e=WA.openPopup("popUpTutorialRect","Hold SHIFT to run !  Up to 4 people at a time can join proximity chat - otherwise for bigger groups join a jitsi call by hanging around the tables.",[])})),WA.onLeaveZone(o,(function(){void 0!==e&&(e.close(),e=void 0)}))})();
 //# sourceMappingURL=script.js.map
